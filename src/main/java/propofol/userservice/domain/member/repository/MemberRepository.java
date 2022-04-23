d13cc224a50a8ed1a5cfa02be954da73c855c2e8
최영찬 <gks54410@gmail.com>
2022-04-23 21:51:16 +0900

    Feat: 회원 이메일, 별명 조회 기능 추가

--- src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
+++ src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package propofol.userservice.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import propofol.userservice.domain.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    Member findDuplicateByNickname(String nickname);
+    Member findDuplicateByEmail(String email);
 }
