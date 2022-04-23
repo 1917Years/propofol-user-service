5b9befc3e6dedd21c6495c39532d8471dc11b0ee
최영찬 <gks54410@gmail.com>
2022-04-23 21:53:57 +0900

    Feat: JpaAuditing 활성화 및 ModelMapper Bean 등록 추가

--- src/main/java/propofol/userservice/UserServiceApplication.java
+++ src/main/java/propofol/userservice/UserServiceApplication.java
@@ -1,13 +1,22 @@
 package propofol.userservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper createModelMapper(){
+		return new ModelMapper();
 	}
 
 }
