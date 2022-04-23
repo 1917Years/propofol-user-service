abb2e3237429fac08e7c4a3c4fd9b8b90fee2867
최영찬 <gks54410@gmail.com>
2022-04-23 21:48:21 +0900

    Feat: 회원 정보 응답 Dto 추가

---
+++ src/main/java/propofol/userservice/api/member/controller/dto/FindMemberDto.java
@@ -0,0 +1,21 @@
+package propofol.userservice.api.member.controller.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class FindMemberDto {
+    private String email;
+    private String username;
+    private String nickname;
+    private String phoneNumber;
+    private LocalDate birth;
+    private String degree;
+    private String score;
+}
