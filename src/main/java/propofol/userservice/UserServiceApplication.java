8fb106dd8810257c82a5dee9db9ed81c57b264e8
최영찬 <gks54410@gmail.com>
2022-04-24 20:16:57 +0900

    Feat: BCryptPasswordEncoder Bean 등록 추가

--- src/main/java/propofol/userservice/UserServiceApplication.java
+++ src/main/java/propofol/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -17,6 +18,11 @@ public class UserServiceApplication {
 	@Bean
 	public ModelMapper createModelMapper(){
 		return new ModelMapper();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder createEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
