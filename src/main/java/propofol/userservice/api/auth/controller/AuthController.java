5ef636d5278de34d3b52b0ec38199e1767b8664b
최영찬 <gks54410@gmail.com>
2022-04-24 20:09:52 +0900

    Feat: 로그인 시 Jwt 토큰 반환 기능 추가

---
+++ src/main/java/propofol/userservice/api/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package propofol.userservice.api.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import propofol.userservice.api.auth.controller.dto.LoginRequestDto;
+import propofol.userservice.api.auth.service.AuthService;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@Slf4j
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public Object login(@RequestBody LoginRequestDto loginDto, HttpServletResponse response){
+        return authService.propofolLogin(loginDto, response);
+    }
+
+
+}
