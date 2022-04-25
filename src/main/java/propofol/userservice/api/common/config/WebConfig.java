8cd126dfd811e10f6aa30c831ea1d668aa3245c2
최영찬 <gks54410@gmail.com>
2022-04-25 22:24:53 +0900

    Feat: TokenResolver 등록

--- src/main/java/propofol/userservice/api/common/config/WebConfig.java
+++ src/main/java/propofol/userservice/api/common/config/WebConfig.java
@@ -1,11 +1,18 @@
 package propofol.userservice.api.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import propofol.userservice.api.common.resolver.TokenResolver;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final TokenResolver tokenResolver;
 
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
@@ -13,4 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowedOrigins("http://localhost:3000")
 //                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
 //    }
+
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+       resolvers.add(tokenResolver);
+    }
 }
