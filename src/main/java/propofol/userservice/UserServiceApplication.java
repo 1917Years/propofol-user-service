7ef39c74bc2b6b22e41576b16bcdeab72f7320b1
최영찬 <gks54410@gmail.com>
2022-04-25 18:32:36 +0900

    Feat: Eureka Client 활성화

--- src/main/java/propofol/userservice/UserServiceApplication.java
+++ src/main/java/propofol/userservice/UserServiceApplication.java
@@ -3,12 +3,14 @@ package propofol.userservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
