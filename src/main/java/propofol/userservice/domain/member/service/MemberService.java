95f4a65a0538147b20c415445782abfdae06f6e9
최영찬 <gks54410@gmail.com>
2022-04-25 22:23:41 +0900

    Feat: 회원 Id 조회 추가

--- src/main/java/propofol/userservice/domain/member/service/MemberService.java
+++ src/main/java/propofol/userservice/domain/member/service/MemberService.java
@@ -2,7 +2,10 @@ package propofol.userservice.domain.member.service;
 
 import propofol.userservice.domain.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberService {
+    Optional<Member> getMemberById(Long id);
     Member getMemberByEmail(String email);
     Boolean checkDuplicateByNickname(String nickname);
     Boolean checkDuplicateByEmail(String email);
