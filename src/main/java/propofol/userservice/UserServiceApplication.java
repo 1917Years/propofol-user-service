9eb43f3345f5a94bf4c9dd59bb9a0d7807caebdf
최영찬 <gks54410@gmail.com>
2022-04-26 22:02:19 +0900

    Feat: RestTemplate Bean 등록

--- src/main/java/propofol/userservice/UserServiceApplication.java
+++ src/main/java/propofol/userservice/UserServiceApplication.java
@@ -3,12 +3,15 @@ package propofol.userservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan(basePackages = "propofol.userservice.api.common.properties")
 //@EnableEurekaClient
 public class UserServiceApplication {
 
@@ -25,5 +28,8 @@ public class UserServiceApplication {
 	public BCryptPasswordEncoder createEncoder(){
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public RestTemplate createRestTemplate() { return new RestTemplate();}
 
 }
