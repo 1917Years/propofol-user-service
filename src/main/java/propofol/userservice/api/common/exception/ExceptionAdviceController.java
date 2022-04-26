57cb5a7c5f87250e1ae036f1541586482f5adce5
최영찬 <gks54410@gmail.com>
2022-04-26 12:56:50 +0900

    Feat: BadRequest Handler 추가

--- src/main/java/propofol/userservice/api/common/exception/ExceptionAdviceController.java
+++ src/main/java/propofol/userservice/api/common/exception/ExceptionAdviceController.java
@@ -2,6 +2,7 @@ package propofol.userservice.api.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,23 +27,33 @@ public class ExceptionAdviceController {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto NotFoundMemberException(NotFoundMember e){
-        ErrorDto errorDto = new ErrorDto();
-        errorDto.setMessage(e.getMessage());
-        errorDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
         return errorDto;
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto validationError(MethodArgumentNotValidException e){
-        ErrorDto errorDto = new ErrorDto();
-        errorDto.setMessage("회원 가입 실패!");
-        errorDto.setStatus(HttpStatus.BAD_REQUEST.value());
+        ErrorDto errorDto = createError("회원 가입 실패!", HttpStatus.BAD_REQUEST);
         e.getFieldErrors().forEach(
                 error -> {
                     errorDto.getErrors().add(new ErrorDetailDto(error.getField(), error.getDefaultMessage()));
                 }
         );
+        return errorDto;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto badRequestType1Error(HttpMessageNotReadableException e){
+        ErrorDto errorDto = createError("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+        return errorDto;
+    }
+
+    private ErrorDto createError(String errorMessage, HttpStatus httpStatus) {
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setMessage(errorMessage);
+        errorDto.setStatus(httpStatus.value());
         return errorDto;
     }
 }
