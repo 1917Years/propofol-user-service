ae8f9b8a4c75564147d4d4e7669dcb9474c63ff6
최영찬 <gks54410@gmail.com>
2022-04-26 21:58:51 +0900

    Feat: KakaoOauth2 설정 정보

---
+++ src/main/java/propofol/userservice/api/common/properties/KakaoOauth2Properties.java
@@ -0,0 +1,27 @@
+package propofol.userservice.api.common.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConfigurationProperties(prefix = "oauth.provider.kakao")
+@ConstructorBinding
+public class KakaoOauth2Properties {
+    private final String getTokenUri;
+    private final String userInfoUri;
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+    private final String grantType;
+
+    public KakaoOauth2Properties(String getTokenUri, String userInfoUri, String clientId,
+                                 String clientSecret, String redirectUri, String grantType) {
+        this.getTokenUri = getTokenUri;
+        this.userInfoUri = userInfoUri;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+        this.grantType = grantType;
+    }
+}
