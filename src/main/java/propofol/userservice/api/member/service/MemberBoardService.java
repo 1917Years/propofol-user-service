6ed0e070fd18c75c447257501688b781f282aaa9
최영찬 <gks54410@gmail.com>
2022-04-28 22:42:20 +0900

    Feat: til-service에서 게시글 정보 받아오는 기능 추가

---
+++ src/main/java/propofol/userservice/api/member/service/MemberBoardService.java
@@ -0,0 +1,19 @@
+package propofol.userservice.api.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import propofol.userservice.api.feign.TilServiceFeignClient;
+import propofol.userservice.api.member.controller.dto.MemberBoardsResponseDto;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberBoardService {
+
+    private final TilServiceFeignClient tilServiceFeignClient;
+
+    public MemberBoardsResponseDto getMyBoards(Integer page, String token){
+        return tilServiceFeignClient.getMyBoards(token, page);
+    }
+}
