9770d0020f009a40370543f28bb97e13854502a6
최영찬 <gks54410@gmail.com>
2022-04-29 14:54:57 +0900

    Feat: 비밀 번호 변경 요청 Dto 추가

---
+++ src/main/java/propofol/userservice/api/auth/controller/dto/UpdatePasswordRequestDto.java
@@ -0,0 +1,15 @@
+package propofol.userservice.api.auth.controller.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class UpdatePasswordRequestDto {
+    @NotBlank(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+    @NotBlank(message = "올바른 비밀번호 형식이 아닙니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 8~15 자리입니다.")
+    private String password;
+}
