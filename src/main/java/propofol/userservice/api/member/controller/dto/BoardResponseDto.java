b3b930c4da28b84b3ad5aa2c563e2af7ff938942
최영찬 <gks54410@gmail.com>
2022-05-14 22:07:37 +0900

    Feat: 게시글 응답 필드명 추가

--- src/main/java/propofol/userservice/api/member/controller/dto/BoardResponseDto.java
+++ src/main/java/propofol/userservice/api/member/controller/dto/BoardResponseDto.java
@@ -3,12 +3,18 @@ package propofol.userservice.api.member.controller.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class BoardResponseDto {
     private Long id;
     private String title;
     private String content;
+    private byte[] imageBytes;
+    private String imageType;
     private Integer recommend;
+    private Integer commentCount;
     private Boolean open;
+    private LocalDateTime createdDate;
 }
