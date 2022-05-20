73c6b7143257201500f22df9e7802cf61c30b79c
최영찬 <gks54410@gmail.com>
2022-05-20 23:07:47 +0900

    Feat: @Jwt 기능 추가

--- src/main/java/propofol/userservice/api/common/config/WebConfig.java
+++ src/main/java/propofol/userservice/api/common/config/WebConfig.java
@@ -3,8 +3,8 @@ package propofol.userservice.api.common.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import propofol.userservice.api.common.resolver.JwtResolver;
 import propofol.userservice.api.common.resolver.TokenResolver;
 
 import java.util.List;
@@ -13,17 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final TokenResolver tokenResolver;
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
-//    }
-
+    private final JwtResolver jwtResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-       resolvers.add(tokenResolver);
+        resolvers.add(tokenResolver);
+        resolvers.add(jwtResolver);
     }
 }
