1e498f2da1e08c3dc0fc70b99f6a0e6575688c69
최영찬 <gks54410@gmail.com>
2022-05-25 23:01:03 +0900

    Feat: 태그 정보 조회 파라미터 변경

--- src/main/java/propofol/userservice/api/feign/service/TagService.java
+++ src/main/java/propofol/userservice/api/feign/service/TagService.java
@@ -3,11 +3,9 @@ package propofol.userservice.api.feign.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import propofol.userservice.api.feign.TagServiceFeignClient;
-import propofol.userservice.api.feign.dto.TagNameDto;
 import propofol.userservice.api.feign.dto.TagsDto;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -15,15 +13,7 @@ public class TagService {
 
     private final TagServiceFeignClient tagServiceFeignClient;
 
-    public List<String> getTagNames(String token, List<Long> ids){
-        TagsDto tagNames = tagServiceFeignClient.getTagNames(token, ids);
-        List<TagNameDto> tags = tagNames.getTags();
-        List<String> list = new ArrayList<>();
-
-        tags.forEach(tag -> {
-            list.add(tag.getName());
-        });
-
-        return list;
+    public TagsDto getTagNames(String token, Set<Long> ids){
+        return tagServiceFeignClient.getTagNames(token, ids);
     }
 }
