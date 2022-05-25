af1f2d245c8e1017247d24cddda5090a845bd7a3
최영찬 <gks54410@gmail.com>
2022-05-25 23:00:41 +0900

    Feat: @RequestParam 키 값 변경

--- src/main/java/propofol/userservice/api/feign/TagServiceFeignClient.java
+++ src/main/java/propofol/userservice/api/feign/TagServiceFeignClient.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import propofol.userservice.api.feign.dto.TagsDto;
 
-import java.util.List;
+import java.util.Set;
 
 @FeignClient("tag-service")
 public interface TagServiceFeignClient {
 
     @GetMapping("api/v1/tags/ids")
     TagsDto getTagNames(@RequestHeader("Authorization") String token,
-                        @RequestParam("id") List<Long> ids);
+                        @RequestParam("ids") Set<Long> ids);
 }
