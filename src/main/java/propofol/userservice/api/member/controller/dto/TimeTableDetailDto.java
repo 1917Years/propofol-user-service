0c665df9b8710aa1ec2ebe173b1ed61474ed4d15
최영찬 <gks54410@gmail.com>
2022-05-28 23:07:59 +0900

    Fix: ModelMapper 기본 생성자 오류 수정

--- src/main/java/propofol/userservice/api/member/controller/dto/TimeTableDetailDto.java
+++ src/main/java/propofol/userservice/api/member/controller/dto/TimeTableDetailDto.java
@@ -2,12 +2,14 @@ package propofol.userservice.api.member.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TimeTableDetailDto {
     private List<String> weeks = new ArrayList<>();
     private List<String> startTimes = new ArrayList<>();
