2337f20e444e7b3780f82a0ddf6c78cbf5eaf317
최영찬 <gks54410@gmail.com>
2022-06-22 12:10:20 +0900

    Feat: 회원 조회 추가

--- src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
+++ src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
@@ -25,7 +25,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.id in :memberIds")
     Page<Member> getMembersByMemberIdsAndPage(@Param("memberIds") Collection<Long> memberIds, Pageable pageable);
 
-    @Query("select m from Member m join fetch m.profile where m.id in :memberIds")
+    @Query("select m from Member m left join fetch m.profile where m.id in :memberIds")
     List<Member> getMembersByMemberIds(@Param("memberIds") Collection<Long> memberIds);
 
     @Query("select m from Member m left join fetch m.memberTags mt where m.id = :memberId")
@@ -35,6 +35,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "order by count(mt) desc, mt.member.totalRecommend desc")
     Page<Member> getMemberTagByTagIds(@Param("tagIds") Collection<Long> tagIds, Pageable pageable);
 
-    @Query("select m from Member m join fetch m.timeTables where m.id = :memberId")
+    @Query("select m from Member m left join fetch m.timeTables where m.id = :memberId")
     Optional<Member> getMemberWithTimeTableByMemberId(@Param("memberId") Long memberId);
+
+    @Query("select mt.member from MemberTag mt where mt.TagId in :tagIds and mt.member.id not in :memberIds " +
+            "group by mt.member.id order by count(mt) desc, mt.member.totalRecommend desc")
+    Page<Member> getMemberTagByTagIdsAndNoMemberIds(@Param("tagIds") Collection<Long> tagIds,
+                                                    @Param("memberIds") Collection<Long> memberIds,
+                                                    Pageable pageable);
 }
